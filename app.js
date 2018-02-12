b5071bd39884e7435c48395af93149ff8d37cc81
Glitch (forest-root) <none>
2018-02-12 15:17:13 +0000

    🎱😺 Checkpoint .env:680347/26 app.js:680347/1

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.post('/webhook', (req, res) => {
 app.get('/webhook', (req, res) => {
   
   /** UPDATE YOUR VERIFY TOKEN **/
-  const VERIFY_TOKEN = "<YOUR_VERIFY_TOKEN>";
+  const VERIFY_TOKEN = "token123";
   
   // Parse params from the webhook verification request
   let mode = req.query['hub.mode'];
