8df9b652e93251ba495ff99ae8a0054e5175b812
Glitch (forest-root) <none>
2018-02-26 13:34:01 +0000

    🚥🎢 Checkpoint app.js:680375/23

--- app.js
+++ app.js
@@ -96,23 +96,25 @@ function handleMessage(sender_psid, received_message) {
 
   let response;
 
-  // Check if the message contains text
-  if (received_message.text) {    
-
-    // Create the payload for a basic text message
+  // Checks if the message contains text
+  if (received_message.text) {
+    
+    // Creates the payload for a basic text message, which
+    // will be added to the body of our request to the Send API
     response = {
-      //"text": `Coucou Julien ravi de te rencontrer ;) (in:"${received_message.text}")`
-      "text": `You sent the message: "${received_message.text}". Now send me an image!`
+      "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
     }
-  }  
+
+  } else if (received_message.attachments) {
+  
+    // Gets the URL of the message attachment
+    let attachment_url = received_message.attachments[0].payload.url;
+    
+  
+  } 
   
   // Sends the response message
   callSendAPI(sender_psid, response);    
-}
-
-// Handles messaging_postbacks events
-function handlePostback(sender_psid, received_postback) {
-
 }
 
 // Sends response messages via the Send API
